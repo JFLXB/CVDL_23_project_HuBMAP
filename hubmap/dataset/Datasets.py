e07bbe2b01f0457fb1b18c1e6c8188de591215f6
jflxb <jonas@blenninger.de>
2023-07-09 22:43:50 +0200

    added reqirements.txt

--- hubmap/dataset/Datasets.py
+++ hubmap/dataset/Datasets.py
@@ -5,7 +5,7 @@ import matplotlib.patches as mpatches
 import matplotlib.pyplot as plt
 import cv2
 import json
-import pandas as pd 
+import pandas as pd
 from abc import ABC, abstractmethod
 
 
@@ -20,14 +20,13 @@ def generate_mask(img_data):
         if group["type"] == "blood_vessel":
             cv2.fillPoly(temp, [points], color=(255))
             mask[:, :, 0] += temp
-        elif group["type"] == "glomerulus":
+        elif group['type'] == "glomerulus":
             cv2.fillPoly(temp, [points], color=(255))
             mask[:, :, 1] += temp
         else:
             cv2.fillPoly(temp, [points], color=(255))
             mask[:, :, 2] += temp
     return mask
-
 
 
 class AbstractDataset(ABC, Dataset):
@@ -38,9 +37,9 @@ class AbstractDataset(ABC, Dataset):
         self.meta_df = pd.read_csv(f"{image_dir}/tile_meta.csv")
 
     def _load_polygons(self):
-        with open(f"{self.image_dir}/polygons.jsonl", "r") as polygons:
+        with open(f'{self.image_dir}/polygons.jsonl', 'r') as polygons:
             json_list = list(polygons)
-        tiles_dicts = [json.loads(json_str) for json_str in json_list]
+        tiles_dicts = [json.loads(json_str)for json_str in json_list]
         return tiles_dicts
 
     def plot_base(self, idx):
@@ -50,19 +49,17 @@ class AbstractDataset(ABC, Dataset):
         image_path = f'{self.image_dir}/train/{img_data["id"]}.tif'
         image = Image.open(image_path)
 
-        legend_elements = [
-            mpatches.Patch(color="green", label="glomerulus"),
-            mpatches.Patch(color="red", label="blood vessel"),
-            mpatches.Patch(color="yellow", label="unsure"),
-        ]
+        legend_elements = [mpatches.Patch(color='green', label='glomerulus'),
+                           mpatches.Patch(color='red', label='blood vessel'),
+                           mpatches.Patch(color='yellow', label='unsure')]
 
         for entry in img_data["annotations"]:
-            if entry["type"] == "glomerulus":
-                color = "green"
-            elif entry["type"] == "blood_vessel":
-                color = "red"
+            if entry["type"] == 'glomerulus':
+                color = 'green'
+            elif entry["type"] == 'blood_vessel':
+                color = 'red'
             else:
-                color = "yellow"
+                color = 'yellow'
 
             sublist = entry["coordinates"][0]
             x = []
@@ -77,9 +74,9 @@ class AbstractDataset(ABC, Dataset):
 
         plt.imshow(image)
         plt.title("Tile with annotations")
-        plt.legend(handles=legend_elements, loc="upper right")
+        plt.legend(handles=legend_elements, loc='upper right')
 
-    #plots a example after transformation i.e. the transformed image and all three masksk
+    # plots a example after transformation i.e. the transformed image and all three masksk
     def plot_example(self, idx):
         img, mask = self[idx]
         img = img.permute(1, 2, 0).numpy()
@@ -88,11 +85,11 @@ class AbstractDataset(ABC, Dataset):
         fig, axs = plt.subplots(1, 4, figsize=(15, 5))
         axs[0].imshow(img)
         axs[0].set_title("Image")
-        axs[1].imshow(mask[:, :, 0], cmap="gray")
+        axs[1].imshow(mask[:, :, 0], cmap='gray')
         axs[1].set_title("blood_vessel mask")
-        axs[2].imshow(mask[:, :, 1], cmap="gray")
+        axs[2].imshow(mask[:, :, 1], cmap='gray')
         axs[2].set_title("glomerulus mask")
-        axs[3].imshow(mask[:, :, 2], cmap="gray")
+        axs[3].imshow(mask[:, :, 2], cmap='gray')
         axs[3].set_title("unsure mask")
 
         plt.tight_layout()
@@ -105,7 +102,6 @@ class AbstractDataset(ABC, Dataset):
     @abstractmethod
     def __getitem__(self, index):
         pass
-
 
 
 # Dataset for all annotated images,
@@ -128,19 +124,18 @@ class BaseDataset(AbstractDataset):
         mask = generate_mask(img_data)
 
         if self.transform is not None:
-            image, mask = self.transform(image, mask) 
-            
+            image, mask = self.transform(image, mask)
+
         return image, mask
-    
+
 
 def _gen_dict_from_json_list(lst):
     out = dict()
-    for json in lst:
-        out[json["id"]] = json
+    for jsn in lst:
+        out[jsn["id"]] = jsn
     return out
 
 
-    
 # Dataset for all EXPERT annotated images (422 tiles),
 # index operations return mask in the following format:
 # [mask_idx, h, w] where mask_dix coresponds to:
@@ -150,7 +145,8 @@ def _gen_dict_from_json_list(lst):
 class ExpertDataset(AbstractDataset):
     def __init__(self, image_dir, transform=None):
         super().__init__(image_dir, transform)
-        self.d1_ids = self.meta_df.loc[self.meta_df['dataset'] == 1]['id'].tolist()
+        self.d1_ids = self.meta_df.loc[self.meta_df['dataset'] == 1]['id'].tolist(
+        )
         self.id_map = _gen_dict_from_json_list(self.tiles_dicts)
 
     def __len__(self):
@@ -166,10 +162,10 @@ class ExpertDataset(AbstractDataset):
             image, mask = self.transform(image, mask)
 
         return image, mask
-    
+
 
 # ich mache nicht die regeln :D
-#https://discuss.pytorch.org/t/torch-utils-data-dataset-random-split/32209
+# https://discuss.pytorch.org/t/torch-utils-data-dataset-random-split/32209
 class DatasetFromSubset(AbstractDataset):
     def __init__(self, subset, transform=None):
         self.subset = subset
