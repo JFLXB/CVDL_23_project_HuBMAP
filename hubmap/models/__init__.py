d6c35ecae4c0ef82d8663e6ab48c18ab191acbf3
Batu <gulbatu3@googlemail.com>
2023-08-18 20:00:07 +0200

    add final stuff

--- hubmap/models/__init__.py
+++ hubmap/models/__init__.py
@@ -3,3 +3,4 @@ from hubmap.models.fct import FCT  # noqa: F401
 from hubmap.models.fct import init_weights  # noqa: F401
 from hubmap.models.ducknet import DUCKNet
 from hubmap.models.ducknet import DUCKNetPretrained, DUCKNetPretrained34
+from hubmap.models.linknet import LinkNet
