059913c137b50289afb680d29abd3a644aa617cc
Batu <gulbatu3@googlemail.com>
2023-08-18 19:50:04 +0200

    add losses

--- hubmap/losses/__init__.py
+++ hubmap/losses/__init__.py
@@ -1,3 +1,4 @@
 from hubmap.losses.bce_dice_loss import BCEDiceLoss  # noqa: F401
 from hubmap.losses.multi_bce import MultiOutputBCELoss  # noqa: F401
 from hubmap.losses.class_weighted_bce import ClassWeightedBCELoss  # noqa: F401
+from hubmap.losses.iou_loss import IoULoss
